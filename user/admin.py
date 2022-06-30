655cc24b9fe9b873fa7510c969a2fedb8f68344f
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-06-30 13:31:47 +0500

    Admin o'zgartirildi

--- user/admin.py
+++ user/admin.py
@@ -1,4 +1,25 @@
 from django.contrib import admin
 from user.models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (None, {
+            'fields':('first_name','last_name','username','profile_photo')
+        }),
+        ('Verified fields',{
+            'fields':(
+                'phone_number',
+                'email'
+            )
+        }),
+        ('Advanced options',{
+            'fields':(
+                'is_admin',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
