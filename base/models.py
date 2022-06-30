fada1d18f6c78000d1b2f451a41df60f435a5cb1
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-06-30 11:49:48 +0500

    Yangililar tasmasi modeli qo'shildi

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Newsletter(models.Model):
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Yangiliklar tasmasi obunachilari'
+
+    def __str__(self):
+        return self.email
+
+    def count_email(self):
+        if self.email is not None:
+            followers = self.email.count()
+        else:
+            pass
