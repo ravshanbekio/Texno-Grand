a3c08e778d72978537905e909bee7446528f3817
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-06-30 15:31:01 +0500

    Deletions

--- product/models.py
+++ product/models.py
@@ -12,16 +12,15 @@ class Category(models.Model):
         return self.category_name
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     name = models.CharField(verbose_name='Mahsulot nomi',max_length=100)
     brand = models.CharField(verbose_name='Brend',max_length=80, blank=True)
     description = models.TextField(verbose_name='Tavsif')
-    model = models.CharField(verbose_name='Mahsulot modeli',max_length=20)
-    cost = models.PositiveIntegerField(verbose_name='Narx',default=100)
+    model = models.CharField(verbose_name='Mahsulot modeli',max_length=100)
+    cost = models.PositiveIntegerField(verbose_name='Narx')
     photo = models.ImageField(verbose_name='Mahsulot rasmi',upload_to='product-photos/', blank=True)
     views = models.IntegerField(default=0)
     like = models.PositiveIntegerField(default=0)
-    product_count = models.PositiveIntegerField(verbose_name='Mahsulot soni', default=10)
 
     class Meta:
         verbose_name_plural = 'Mahsulotlar'
